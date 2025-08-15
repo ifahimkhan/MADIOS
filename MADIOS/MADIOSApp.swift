3567daafb61b72ea3dda0b13b5bd4ca9423f9dfa
ifahimkhan <khan.fahim95@hotmail.com>
2025-08-15 23:57:43 +0530

    Initial Commit

---
+++ MADIOS/MADIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MADIOSApp.swift
+//  MADIOS
+//
+//  Created by apple on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MADIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MADIOSTests/MADIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MADIOSTests.swift
+//  MADIOSTests
+//
+//  Created by apple on 8/15/25.
+//
+
+import Testing
+@testable import MADIOS
+
+struct MADIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
